1da6931a9b2b7d2d3be378b7ad01011cfccc6d46
2ndkite <98459226+2ndkite@users.noreply.github.com>
2022-01-29 19:40:09 +0900

    1004 little prince
    https://www.acmicpc.net/problem/1004

---
+++ 1004.py
@@ -0,0 +1,12 @@
+t = int(input())
+for i in range(t):
+    cnt = 0
+    x1, y1, x2, y2 = map(int, input().split())
+    n = int(input())
+    for i in range(n):
+        cx, cy, cr = map(int, input().split())
+        d1 = (((x1 - cx) ** 2) + ((y1 - cy) ** 2)) ** 0.5
+        d2 = (((x2 - cx) ** 2) + ((y2 - cy) ** 2)) ** 0.5
+        if (d1 < cr and d2 > cr) or (d1 > cr and d2 < cr):
+            cnt += 1
+    print(cnt)
