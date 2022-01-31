e20ab22ab4a64f3b08a5fe768d9e6949c4daa559
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-01-31 14:41:58 +0900

    10814
    https://www.acmicpc.net/problem/10814

---
+++ 10814.py
@@ -0,0 +1,7 @@
+import sys
+arr = []
+for i in range(int(sys.stdin.readline())):
+    arr.append(list(sys.stdin.readline().split()))
+arr.sort(key=lambda x: int(x[0]))
+for i in arr:
+    print(i[0], i[1])
