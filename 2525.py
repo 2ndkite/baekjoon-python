bbe981893c07c1dcec48e365bcb2c9e63b41b79e
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-02-02 14:48:31 +0900

    2525 oven timer
    https://www.acmicpc.net/problem/2525

---
+++ 2525.py
@@ -0,0 +1,14 @@
+import sys
+H, M = map(int, sys.stdin.readline().split())
+timer = int(sys.stdin.readline()) 
+
+H += timer // 60
+M += timer % 60
+
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H,M)
