c0bed8408f80e514b92fbef9508a5614a78c9c8c
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-02-03 19:23:10 +0900

    2476
    https://www.acmicpc.net/problem/2476

---
+++ 2476.py
@@ -0,0 +1,17 @@
+import sys
+n = int(sys.stdin.readline())
+answer = 0
+for i in range(n):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == b == c:
+        answer = max(answer, 10000+a*1000)
+    elif a == b:
+        answer = max(answer, 1000+a*100)
+    elif a == c:
+        answer = max(answer, 1000+a*100)
+    elif b == c:
+        answer = max(answer, 1000+b*100)
+    else:
+        answer = max(answer, 100 * max(a,b,c))
+
+print(answer)
