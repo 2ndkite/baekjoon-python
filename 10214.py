3c95ea2351de550b452d37ee9d99b390d4bb53cf
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-02-05 18:25:00 +0900

    10214 Baseball
    https://www.acmicpc.net/problem/10214

---
+++ 10214.py
@@ -0,0 +1,15 @@
+import sys
+t = int(sys.stdin.readline())
+for i in range(t):
+    y = 0
+    k = 0
+    for i in range(9):
+        a, b = map(int, sys.stdin.readline().split())
+        y += a
+        k += b
+    if y > k:
+        print("Yonsei")
+    elif y < k:
+        print("Korea")
+    else:
+        print("Draw")
