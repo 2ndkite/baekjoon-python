b9d8a91a15833d7cab3da3a21db624a727796cf1
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-03-28 01:30:16 +0900

    10448
    https://www.acmicpc.net/problem/10448

---
+++ 10448.py
@@ -0,0 +1,11 @@
+import sys
+triangle = [n*(n+1)//2 for n in range(1, 46)]
+eureka = [0] * 1001
+for i in triangle:
+    for j in triangle:
+        for k in triangle:
+            if i+j+k <= 1000:
+                eureka[i+j+k] = 1
+                
+for i in range(int(sys.stdin.readline())):
+    print(eureka[int(sys.stdin.readline())])
