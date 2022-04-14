d9bf5d70d037feed31dcb5fc07d6d25094419dd8
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-04-14 20:59:08 +0900

    1173
    https://www.acmicpc.net/problem/1173

---
+++ 1173.py
@@ -0,0 +1,14 @@
+import sys
+N, m, M, T, R = map(int, sys.stdin.readline().split())
+cnt = t = 0
+now = m
+while cnt < N:
+    if m+T > M:
+        break
+    if now + T <= M:
+        now += T
+        cnt += 1
+    else:
+        now = max(now-R, m)
+    t += 1
+print(t if cnt == N else -1)
