523e31aa586a8f448c09fc59cdc6dab42f32c2d7
Daniel Ho Yeon Lee <98459226+2ndkite@users.noreply.github.com>
2022-04-14 20:57:12 +0900

    1247
    https://www.acmicpc.net/problem/1247

---
+++ 1247.py
@@ -0,0 +1,10 @@
+import sys
+for i in range(3):
+    N = int(sys.stdin.readline())
+    li = [int(sys.stdin.readline()) for i in range(N)]
+    if sum(li) == 0:
+        print("0")
+    elif sum(li) > 0:
+        print("+")
+    else:
+        print("-")
